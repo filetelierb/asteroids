7c37b4f461fdcae64daafa679f20819d2789a29e
feletelier <felipe@igniuslab.com>
2025-08-27 10:24:28 +0200

    Added clock/FPS

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        
         screen.fill("black")
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
