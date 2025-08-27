4fb13876783dd8a3ae76f3de6616bf3507c90dc8
feletelier <felipe@igniuslab.com>
2025-08-27 18:38:13 +0200

    Added shot method and bullets

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import *
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
 
@@ -28,6 +29,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def rotate(self,dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -36,3 +39,9 @@ class Player(CircleShape):
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+    
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        shot.velocity = forward * PLAYER_SHOOT_SPEED
+        
