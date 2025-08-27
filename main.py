22af261504127d7ba9738d3d2c891c9c8779d6be
feletelier <felipe@igniuslab.com>
2025-08-27 10:20:06 +0200

    Fix flip implementation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill("black")
-        screen.flip()
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
