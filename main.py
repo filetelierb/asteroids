6a6a216a3a00b0b28db788fa511ab08b3ba7993c
feletelier <felipe@igniuslab.com>
2025-08-27 10:37:19 +0200

    Added circle/player classes

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+
     clock = pygame.time.Clock()
     dt = 0
-    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
