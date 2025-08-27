a330a361949c94c2d38212ca55439334ea95570d
feletelier <felipe@igniuslab.com>
2025-08-27 10:42:51 +0200

    Fix main rotate bug

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         
         screen.fill("black")
+        player.rotate(dt)
         player.draw(screen)
         pygame.display.flip()
         
